f6aa634d767d091f38f39438e16648ae755885a1
Cody Hill <codyhill@google.com>
2022-05-11 14:03:37 +0000

    more

--- variables.tf
+++ variables.tf
@@ -3,43 +3,52 @@ variable "cluster_name" {
   type        = string
   default     = "my-cluster"
 }
+
 variable "cloud" {
   description = "The Cloud to deploy the Baremetal cluster on"
   type        = string
   default     = "PNAP"
 }
+
 variable "pnap_client_id" {
   description = "The client id for authentication to pnap"
   type        = string
 }
+
 variable "pnap_client_secret" {
   description = "The client secret for authentication to pnap"
   type        = string
 }
+
 variable "pnap_location" {
   description = "The pnap region to deploy nodes to"
   type        = string
   default     = "PHX"
 }
+
 variable "pnap_worker_type" {
   description = "The type of PNAP server to deploy for worker nodes"
   type        = string
   default     = "s2.c1.medium"
 }
+
 variable "pnap_cp_type" {
   description = "The type of PNAP server to deploy for control plane nodes"
   type        = string
   default     = "s2.c1.medium"
 }
+
 variable "gcp_project_id" {
   description = "The GCP Project ID"
   type        = string
 }
+
 variable "ansible_url" {
   description = "The Ansible URL for the Anthos Automation"
   type        = string
   default     = "https://github.com/GCPartner/ansible-gcpartner-anthos-baremetal/archive/refs/heads/v0.0.1.tar.gz"
 }
+
 variable "ansible_tar_ball" {
   description = "The name of the ansible tarball"
   type        = string
@@ -75,6 +84,7 @@ variable "gcp_router_asn" {
   type        = number
   default     = 16550
 }
+
 variable "megaport_requested_asn" {
   description = "The requested asn for Megaport"
   type        = number
